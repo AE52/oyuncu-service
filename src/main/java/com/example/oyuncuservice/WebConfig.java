c443ea3c127e1cbcb1230e9cefcd563af7be82f9
ae52 <eren.sistemmep@gmail.com>
2024-12-28 23:03:25 +0300

    cors

---
+++ src/main/java/com/example/oyuncuservice/WebConfig.java
@@ -0,0 +1,19 @@
+package com.example.oyuncuservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class WebConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // "/api/**" yoluna gelen tüm istekler için CORS'u etkinleştir
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000", "https://oyuncu-service-597129445396.us-central1.run.app") // Frontend domainlerinizi burada belirtin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // İzin verilen HTTP metotları
+                .allowedHeaders("*"); // İzin verilen başlıklar
+    }
+}
