35b5ed85148d03682c802a1b9228bf005a5472c4
ae52 <eren.sistemmep@gmail.com>
2024-12-29 22:06:29 +0300

    cors set

--- src/main/java/com/example/oyuncuservice/WebConfig.java
+++ src/main/java/com/example/oyuncuservice/WebConfig.java
@@ -2,18 +2,24 @@ package com.example.oyuncuservice;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.CorsRegistry;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
-public class WebConfig implements WebFluxConfigurer {
+public class WebConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**") // sadece "/api" yolundaki istekler için
-                .allowedOrigins("https://oyuncu-service-597129445396.us-central1.run.app", "http://localhost:3000") // doğru frontend domainler
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // izin verilen HTTP metotları
-                .allowedHeaders("*");
-
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("https://oyuncu-service-597129445396.us-central1.run.app");
+        configuration.addAllowedOrigin("http://oyuncu-service-597129445396.us-central1.run.app");
+        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(false); // Eğer kimlik doğrulama gerekiyorsa
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api/**", configuration);
+        return source;
     }
 }
